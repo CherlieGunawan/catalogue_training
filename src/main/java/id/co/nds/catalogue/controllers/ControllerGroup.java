26f68d0e131a2b1b603040a8925badd96e6f7686
Cherlie Gunawan <cherliegunawan@gmail.com>
2022-08-12 13:35:38 +0700

    forgot to push ControllerGroup

---
+++ src/main/java/id/co/nds/catalogue/controllers/ControllerGroup.java
@@ -0,0 +1,15 @@
+package id.co.nds.catalogue.controllers;
+
+public class ControllerGroup {
+    public interface PostingNew {}
+
+    public interface GettingAllByCriteria {}
+
+    public interface GettingById {}
+
+    public interface UpdatingById extends RequestMethodById {}
+
+    public interface DeletingById extends RequestMethodById {}
+
+    public interface RequestMethodById {}
+}
